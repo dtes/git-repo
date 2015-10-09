dc9753c6d2d8cda047d90c89fb2e641789b67959
dtes <mygrnail@gmail.com>
2015-10-09 17:38:24 +0600

    hello everbody

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Hello world!");
 		System.out.println("How are you?");
+		System.out.println("Hello everybody!");
 	}
 }
